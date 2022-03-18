54be1b45bd3a659297c258bfb02304b11985ba26
Kimdonguk <kimdonguk@Kimdongukui-MacBookPro.local>
2022-03-19 00:55:41 +0900

    Add modal

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,31 +1,38 @@
 <template>
   <section>
-      <input type="text" v-model="newTodoItem" @keyup.enter="addTodo">
-      <button @click="addTodo">add todo</button>
+    <input type="text" v-model="newTodoItem" @keyup.enter="addTodo" />
+    <button @click="addTodo">add todo</button>
+    <modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">custom header</h3>
+    </modal>
+    <button id="show-modal" @click="showModal = true">Show Modal</button>
   </section>
 </template>
 
 <script>
+import Modal from "./common/Modal.vue";
+
 export default {
-    data: function() {
-        return {
-            newTodoItem: ""
-        }
+  components: {
+    Modal,
+  },
+  data: function () {
+    return {
+      newTodoItem: "",
+    };
+  },
+  methods: {
+    addTodo: function () {
+      if (this.newTodoItem !== "") {
+        this.$emit("addTodoItem", this.newTodoItem);
+        this.clearInput();
+      }
     },
-    methods: {
-        addTodo: function() {
-            if(this.newTodoItem !== ""){
-                this.$emit("addTodoItem", this.newTodoItem)
-                this.clearInput()
-            }
-        },
-        clearInput: function(){
-            this.newTodoItem = "";
-        }
-    }
-}
+    clearInput: function () {
+      this.newTodoItem = "";
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
