1b362d75c02034f50571cf2a978ce8b68dbf9f9e
Kimdonguk <kimdonguk@Kimdongukui-MacBookPro.local>
2022-03-19 12:20:43 +0900

    Improv modal

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -2,23 +2,28 @@
   <section>
     <input type="text" v-model="newTodoItem" @keyup.enter="addTodo" />
     <button @click="addTodo">add todo</button>
-    <modal v-if="showModal" @close="showModal = false">
-      <h3 slot="header">custom header</h3>
-    </modal>
+    <modal-comp v-if="showModal">
+      <h3 slot="header">경고</h3>
+      <h5 slot="body">글 적으셈</h5>
+      <p slot="footer">
+          <button class="modal-default-button" @click="showModal = false"></button>
+      </p>
+    </modal-comp>
     <button id="show-modal" @click="showModal = true">Show Modal</button>
   </section>
 </template>
 
 <script>
-import Modal from "./common/Modal.vue";
+import ModalComp from "./common/ModalComp.vue";
 
 export default {
   components: {
-    Modal,
+    ModalComp,
   },
   data: function () {
     return {
       newTodoItem: "",
+      showModal: false
     };
   },
   methods: {
@@ -26,7 +31,10 @@ export default {
       if (this.newTodoItem !== "") {
         this.$emit("addTodoItem", this.newTodoItem);
         this.clearInput();
+        return
       }
+
+      this.showModal = !this.showModal
     },
     clearInput: function () {
       this.newTodoItem = "";
